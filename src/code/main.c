67492f629d538ce36832c40962f51344776bdb91
Maxim Luparev <luparev22@list.ru>
2019-04-16 21:42:28 +0700

    Created main window

--- src/code/main.c
+++ src/code/main.c
@@ -1,4 +1,17 @@
+#include <gtk/gtk.h>
+
 int main(int argc, char* argv[])
 {
+    GtkWidget* window;
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "XEngl");
+    gtk_widget_set_size_request(GTK_WIDGET(window), 600, 700);
+    gtk_container_set_border_width(GTK_CONTAINER(window), 0);
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+    g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(exit), (gpointer)0);
+
+    gtk_widget_show_all(window);
+    gtk_main();
     return 0;
 }
