c96725eaf0d0175272d105440af0595c3e44b13c
Maxim Luparev <luparev22@list.ru>
2019-04-16 21:50:57 +0700

    Added button to exit the app

--- src/code/main.c
+++ src/code/main.c
@@ -2,11 +2,12 @@
 
 int main(int argc, char* argv[])
 {
-    GtkWidget *window, *main_box, *button_box;
+    GtkWidget *window, *main_box, *button_box, *main_buttons[4];
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     button_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 25);
+    main_buttons[0] = gtk_button_new_with_label("Выход");
     gtk_window_set_title(GTK_WINDOW(window), "XEngl");
     gtk_widget_set_size_request(GTK_WIDGET(window), 600, 700);
     gtk_container_set_border_width(GTK_CONTAINER(window), 0);
@@ -14,6 +15,7 @@ int main(int argc, char* argv[])
 
     g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(exit), (gpointer)0);
 
+    gtk_box_pack_start(GTK_BOX(button_box), main_buttons[0], TRUE, TRUE, 0);
     gtk_box_pack_end(GTK_BOX(main_box), button_box, TRUE, TRUE, 0);
 
     gtk_container_add(GTK_CONTAINER(window), main_box);
