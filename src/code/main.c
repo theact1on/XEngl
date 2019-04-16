828b671efb5c565edfa095eb521c3a54b8d52504
Maxim Luparev <luparev22@list.ru>
2019-04-16 21:44:18 +0700

    Added container main_box

--- src/code/main.c
+++ src/code/main.c
@@ -2,15 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-    GtkWidget* window;
+    GtkWidget *window, *main_box;
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_window_set_title(GTK_WINDOW(window), "XEngl");
     gtk_widget_set_size_request(GTK_WIDGET(window), 600, 700);
     gtk_container_set_border_width(GTK_CONTAINER(window), 0);
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
     g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(exit), (gpointer)0);
 
+    gtk_container_add(GTK_CONTAINER(window), main_box);
     gtk_widget_show_all(window);
     gtk_main();
     return 0;
