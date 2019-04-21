df168bf3e159d7d15ef259cff4187bb9377917cb
Maxim Luparev <luparev22@list.ru>
2019-04-21 22:34:06 +0700

    Added binary file parsing algorithm

--- src/code/stats_win.c
+++ src/code/stats_win.c
@@ -6,6 +6,7 @@ void stats_win(GtkWidget* widget, gpointer data)
     GtkWidget *stats_box, *btn_box, *btn_back, *btn_del_stats, *treeview, *scrolled_win, *label;
     GtkWidget* main_box = g_object_ref((GtkWidget*)gtk_bin_get_child(GTK_BIN(window)));
     GtkListStore* store;
+    GtkTreeIter iter;
 
     gtk_container_remove(GTK_CONTAINER(window), main_box);
     stats_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
@@ -38,6 +39,29 @@ void stats_win(GtkWidget* widget, gpointer data)
     store = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_INT, G_TYPE_INT, G_TYPE_INT);
     gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), GTK_TREE_MODEL(store));
     gtk_container_add(GTK_CONTAINER(scrolled_win), treeview);
+
+    FILE* stats = fopen("data/stats.dat", "rb");
+    if (stats == NULL) {
+        stats = fopen("data/stats.dat", "wb");
+        fclose(stats);
+        stats = fopen("data/stats.dat", "rb");
+    }
+
+    struct stats_rec* user_stats;
+    user_stats = (struct stats_rec*)malloc(sizeof(struct stats_rec));
+
+    fread(user_stats, sizeof(struct stats_rec), 1, stats);
+    if (feof(stats)) {
+        gtk_widget_set_sensitive(btn_del_stats, FALSE);
+    }
+    while (stats != NULL && !feof(stats)) {
+        gtk_list_store_prepend(store, &iter);
+        gtk_list_store_set(store, &iter, 0, user_stats->date, 1, user_stats->suc_words, 2, user_stats->all_words, 3, user_stats->suc_words / user_stats->all_words * 100, -1);
+        gtk_main_iteration_do(gtk_events_pending());
+        fread(user_stats, sizeof(struct stats_rec), 1, stats);
+    }
+    fclose(stats);
+    stats = NULL;
 
     gtk_widget_show_all(stats_box);
 
--- src/code/stats_win.h
+++ src/code/stats_win.h
@@ -2,6 +2,11 @@
 #define STATS_WIN_H
 #include <gtk/gtk.h>
 #include <stdlib.h>
+struct stats_rec {
+    char date[20];
+    int suc_words;
+    int all_words;
+};
 void stats_win(GtkWidget* widget, gpointer data);
 void setup_table(GtkWidget* treeview);
 #endif
