9cc79ea9146dfcb01f869b1a3564df782b4617fe
Maxim Luparev <luparev22@list.ru>
2019-04-21 21:12:57 +0700

    Created function stats_win

--- src/code/stats_win.c
+++ src/code/stats_win.c
@@ -1,1 +1,5 @@
 #include "stats_win.h"
+
+void stats_win(GtkWidget* widget, gpointer data)
+{
+}
--- src/code/stats_win.h
+++ src/code/stats_win.h
@@ -1,4 +1,6 @@
 #ifndef STATS_WIN_H
 #define STATS_WIN_H
-
+#include <gtk/gtk.h>
+#include <stdlib.h>
+void stats_win(GtkWidget* widget, gpointer data);
 #endif
