fe2d135e8973f6eb0139c643e3b77ffd97d15916
Maxim Luparev <luparev22@list.ru>
2019-05-03 00:59:25 +0700

    Added change window alghoritm

--- src/code/training.c
+++ src/code/training.c
@@ -8,5 +8,13 @@ void training_win(GtkWidget* widget, gpointer data)
     GtkWidget *tr_box, *label_name_window, *task_box, *button_box, *btn_next, *btn_end;
     GtkWidget* main_box = g_object_ref((GtkWidget*)gtk_bin_get_child(GTK_BIN(window)));
 
-    gtk_main_quit();
+    gtk_container_remove(GTK_CONTAINER(window), main_box);
+    tr_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    gtk_container_add(GTK_CONTAINER(window), tr_box);
+
+    gtk_main();
+
+    gtk_widget_destroy(tr_box);
+    gtk_container_add(GTK_CONTAINER(window), main_box);
+    gtk_widget_show_all(window);
 }
