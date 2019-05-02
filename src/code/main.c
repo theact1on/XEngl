b937eef98c48daf734eb9b2b28ea221bdc542561
Maxim Luparev <luparev22@list.ru>
2019-05-03 01:07:25 +0700

    Added button 'Start' in main menu

--- src/code/main.c
+++ src/code/main.c
@@ -1,4 +1,5 @@
 #include "stats_win.h"
+#include "training.h"
 #include "vocabulary.h"
 #include <gtk/gtk.h>
 #include <stdlib.h>
@@ -9,6 +10,7 @@ int main(int argc, char* argv[])
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     main_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     button_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 25);
+    main_buttons[0] = gtk_button_new_with_label("Начать");
     main_buttons[1] = gtk_button_new_with_label("Словарь");
     main_buttons[2] = gtk_button_new_with_label("Статистика");
     main_buttons[3] = gtk_button_new_with_label("Выход");
@@ -20,12 +22,14 @@ int main(int argc, char* argv[])
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
     g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(exit), (gpointer)0);
+    g_signal_connect(G_OBJECT(main_buttons[0]), "clicked", G_CALLBACK(training_win), window);
     g_signal_connect(G_OBJECT(main_buttons[1]), "clicked", G_CALLBACK(vocabulary_win), window);
     g_signal_connect(G_OBJECT(main_buttons[2]), "clicked", G_CALLBACK(stats_win), window);
     g_signal_connect_swapped(G_OBJECT(main_buttons[3]), "clicked", G_CALLBACK(exit), (gpointer)0);
 
     gtk_box_pack_start(GTK_BOX(main_box), main_logo, FALSE, FALSE, 0);
     gtk_box_pack_end(GTK_BOX(main_box), button_box, TRUE, TRUE, 0);
+    gtk_box_pack_start(GTK_BOX(button_box), main_buttons[0], TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(button_box), main_buttons[1], TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(button_box), main_buttons[2], TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(button_box), main_buttons[3], TRUE, TRUE, 0);
